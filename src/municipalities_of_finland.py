0064304714cee8edd078ad209da97ad1c935001f
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:52:55 -0400

    init

---
+++ src/municipalities_of_finland.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def municipalities_of_finland():
+    pass
+    
+def main():
+    print(municipalities_of_finland())
+    
+if __name__ == "__main__":
+    main()
